d7bc7ddfbf50b95a86eafcd57bb51444bd91f2b7
samyeid778 <aytam1196suraa@gmail.com>
2026-08-14 20:03:46 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask( __name__)
+app = Flask(name)
+
+VERIFY_TOKEN = "samy123"
 
 @app.route("/")
 def home():
     return "Webhook Running"
 
-if  __name__ == "__main__":
+
+@app.route("/webhook", methods=["GET"])
+def verify_webhook():
+
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+
+    return "Verification failed", 403
+
+
+@app.route("/webhook", methods=["POST"])
+def receive_webhook():
+
+    data = request.json
+
+    print("Received Webhook:")
+    print(data)
+
+    return jsonify(
+        {
+            "status": "success"
+        }
+    ), 200
+
+
+if name == "main":
     app.run(host="0.0.0.0", port=10000)
