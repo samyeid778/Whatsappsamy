db98bd7deefc77cbbeba5e49f095745ee0472686
samyeid778 <aytam1196suraa@gmail.com>
2026-08-14 19:57:17 +0300

    Fix Flask app initialization and main check

--- webhook.py
+++ webhook.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask( __name__)
 
 @app.route("/")
 def home():
     return "Webhook Running"
 
-if name == "main":
+if  __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
