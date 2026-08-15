b6401bda7402b17491a39a6a5e58b17d53f4586d
samyeid778 <aytam1196suraa@gmail.com>
2026-08-15 10:08:30 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, jsonify
-import json
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -19,8 +17,6 @@ def verify_webhook():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
 
-    print("VERIFY REQUEST RECEIVED")
-
     if mode == "subscribe" and token == VERIFY_TOKEN:
         return challenge, 200
 
@@ -33,12 +29,31 @@ def receive_webhook():
     raw_data = request.get_data(as_text=True)
 
     with open("payload.txt", "a", encoding="utf-8") as f:
-        f.write(raw_data)
-        f.write("\n\n====================\n\n")
 
-    return jsonify({"status": "ok"}), 200
+        f.write(raw_data)
+        f.write("\n\n========================\n\n")
+
+    return jsonify(
+        {
+            "status": "ok"
+        }
+    ), 200
+
+
+@app.route("/payload")
+def show_payload():
+
+    try:
+
+        with open("payload.txt", "r", encoding="utf-8") as f:
+            return f.read()
+
+    except Exception:
+        return "No payload yet"
+
 
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=10000
